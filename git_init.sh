7626dd968be68a2b91f673ff09ad31dfeaaa0e6a
fgwsz <3191000458@qq.com>
2026-06-11 20:57:37 +0800

    修正git_init.sh仓库url错误

--- git_init.sh
+++ git_init.sh
@@ -13,5 +13,5 @@ echo "# $project_path" > README.md
 git add README.md
 git commit -m "first commit"
 git branch -M main
-git remote add origin "git@github.com:$user_name/$project_path.git"
+git remote add origin "git@github.com:$user_name/$project_name.git"
 git push -u origin main
